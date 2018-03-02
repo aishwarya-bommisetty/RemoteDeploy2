ca8d78e748d964e8fc11fa941354b2cd8e70ff3f
aishwaryaaaaaaa <aishwarya.bommisetty@gmail.com>
2018-03-02 11:55:36 +0530

    update deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh ggk@54.244.209.186 < test_script.sh >> f1.txt
+ssh ggk@54.244.209.186 < app/test_script.sh >> f1.txt
 cd SpringMVCSecurityXML
 mvn install >> f1.txt
 mv target/SpringMVCSecurityXML.war target/aishwarya.war
