f46c319ae96c400c25c6d572fd1fc1a84c1ab172
aishwaryaaaaaaa <aishwarya.bommisetty@gmail.com>
2018-03-02 12:01:15 +0530

    update2 : removed build

--- Deploy.sh
+++ Deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 ssh ggk@54.244.209.186 < app/test_script.sh >> f1.txt
-cd SpringMVCSecurityXML
-mvn install >> f1.txt
 mv target/SpringMVCSecurityXML.war target/aishwarya.war
 scp target/aishwarya.war ggk@54.244.209.186:/var/lib/tomcat8/webapps
 echo "DEPLOYED SUCCESSFULLY"
