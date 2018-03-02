2c6e78ac2618055efa32e47fcddab20da71ef8ef
aishwaryaaaaaaa <aishwarya.bommisetty@gmail.com>
2018-03-02 12:03:01 +0530

    updated again

--- Deploy.sh
+++ Deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ssh ggk@54.244.209.186 < app/test_script.sh >> f1.txt
+cd SpringMVCSecurityXML
 mv target/SpringMVCSecurityXML.war target/aishwarya.war
 scp target/aishwarya.war ggk@54.244.209.186:/var/lib/tomcat8/webapps
 echo "DEPLOYED SUCCESSFULLY"
